7de71b0e711fbe529df7b16e53eac508276cc1eb
frankdejonge <info@frenky.net>
2016-02-12 16:53:59 +0100

    Close connection goroutines when a connection is closed.

--- connection.go
+++ connection.go
@@ -3,48 +3,78 @@ package main
 import (
 	"fmt"
 	"github.com/gorilla/websocket"
+	"log"
 )
 
 type Connection struct {
 	ws       *websocket.Conn
 	messages chan []byte
+	cancel   chan bool
+}
+
+func broadcastMessage(messageType int, message []byte) {
+	switch messageType {
+	case websocket.BinaryMessage:
+	case websocket.TextMessage:
+		fmt.Println("Broadcasting message", string(message))
+		hub.Broadcast(message)
+	default:
+		fmt.Println("Unknown message", messageType, message)
+	}
+}
+
+func handleConnectionError(connection *Connection, err error) {
+	if websocket.IsUnexpectedCloseError(err, websocket.CloseGoingAway, websocket.CloseNormalClosure) {
+		log.Printf("Unexpected error from connection: %q", err)
+	}
+
+	hub.Unregister(connection);
 }
 
 func (connection *Connection) WriteMessages(hub *Hub) {
 	for {
-		message := <-connection.messages
-		connection.ws.WriteMessage(websocket.TextMessage, message)
+		select {
+		case <-connection.cancel:
+			return
+		default:
+			message := <-connection.messages
+			err := connection.ws.WriteMessage(websocket.TextMessage, message)
+
+			if err != nil {
+				handleConnectionError(connection, err)
+				return
+			}
+		}
 	}
 }
 
 func (connection *Connection) ReadMessages(hub *Hub) {
 	for {
-		messageType, message, err := connection.ws.ReadMessage()
-
-		if err != nil {
+		select {
+		case <-connection.cancel:
 			break
-		}
-
-		switch messageType {
-		case websocket.BinaryMessage:
-		case websocket.TextMessage:
-			fmt.Println("Broadcasting message", string(message))
-			hub.Broadcast(message)
 		default:
-			fmt.Println("Unknown message", messageType, message)
-			break
+			messageType, message, err := connection.ws.ReadMessage()
+
+			if err != nil {
+				handleConnectionError(connection, err)
+				return
+			} else {
+				broadcastMessage(messageType, message);
+			}
 		}
 	}
-	hub.unregister <- connection
 }
 
 func (connection *Connection) Close() {
 	connection.ws.Close()
+	connection.cancel <- true
 }
 
 func NewConnection(ws *websocket.Conn) *Connection {
 	return &Connection{
 		ws: ws,
 		messages: make(chan []byte, 256),
+		cancel: make(chan bool, 1),
 	}
 }
